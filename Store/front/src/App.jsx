93d513a66e2ce2dd43b91fee12e6133f5d6b37e9
HimanshuAdhikari <adhiakrihimanshu393@gmail.com>
2024-06-10 17:24:31 +0530

    basic structure

---
+++ Store/front/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Banner from './component/Banner'
+import Foot from './component/Foot'
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+      <Foot/>
+    </div>
+  )
+}
+
+export default App
